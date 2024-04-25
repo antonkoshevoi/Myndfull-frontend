5faf44efcd07dc564a05043b4bdfd8bee0f4d449
antonkoshevoi <anton.koshevoi.dev@gmail.com>
2024-04-25 16:46:05 +0300

    fix: app_routec added success signin

--- cypress/e2e/test-examples/app_routes.cy.js
+++ cypress/e2e/test-examples/app_routes.cy.js
@@ -1,47 +1,52 @@
-describe('AppRoutes', () => {
-    it('successfully loads', () => {
-        cy.visit('http://localhost:3000');
-    });
-    it('should contain About Us and Sign in buttons', () => {
-        cy.visit('/');
+describe("AppRoutes", () => {
+  it("successfully loads", () => {
+    cy.visit("http://localhost:3000");
+  });
+  it("should contain About Us and Sign in buttons", () => {
+    cy.visit("/");
 
-        cy.contains('About us').should('be.visible');
-        cy.contains('Sign in').should('be.visible');
-    });
+    cy.contains("About us").should("be.visible");
+    cy.contains("Sign in").should("be.visible");
+  });
 
-    it('should contain a h2 with text "Little story about the company"', () => {
-        cy.visit('/');
+  it('should contain a h2 with text "Little story about the company"', () => {
+    cy.visit("/");
 
-        cy.contains('Little story about the company').should('be.visible');
-    });
+    cy.contains("Little story about the company").should("be.visible");
+  });
 
-    it('should navigate to SignIn page when clicking "Sign in" button', () => {
-        cy.visit('/');
-        cy.contains('Sign in').click();
-        cy.url().should('include', '/signin');
-    });
+  it('should navigate to SignIn page when clicking "Sign in" button', () => {
+    cy.visit("/");
+    cy.contains("Sign in").click();
+    cy.url().should("include", "/signin");
+  });
 
-    it('should be able to login', () => {
-        cy.visit('/signin');
-        cy.get('#email').type('your-email@example.com');
-        cy.get('#email').should('have.value', 'your-email@example.com');
-        cy.get('#password').type('12345');
-        cy.get('#password').should('have.value', '12345');
-        cy.contains('Submit').click();
+  it("should be able to login", () => {
+    cy.visit("/signin");
+    cy.get("#email").type("your-email@example.com");
+    cy.get("#email").should("have.value", "your-email@example.com");
+    cy.get("#password").type("12345");
+    cy.get("#password").should("have.value", "12345");
+    cy.contains("Submit").click();
+    cy.contains("Access denied.");
 
+    cy.get("#email").clear().type("aleksei@example.com");
+    cy.get("#email").should("have.value", "aleksei@example.com");
+    cy.get("#password").clear().type("lkJlkn8hj");
+    cy.get("#password").should("have.value", "lkJlkn8hj");
+    cy.contains("Submit").click();
 
-        cy.url().should('include', '/profile');
-        cy.contains('Welcome,');
-        cy.contains('Update');
-        cy.contains('About us');
-        cy.contains('Profile');
-        cy.contains('Sign out');
-        cy.get('img[src="logo.png"]').should('exist');
+    cy.url().should("include", "/profile");
+    cy.contains("Welcome,");
+    cy.contains("Update");
+    cy.contains("About us");
+    cy.contains("Profile");
+    cy.contains("Sign out");
+    cy.get('img[src="logo.png"]').should("exist");
 
-        cy.contains('Sign out').click();
-        cy.url().should('not.include', '/profile');
-        cy.contains('Sign in').should('exist');
-        cy.contains('About us');
-    });
-
+    cy.contains("Sign out").click();
+    cy.url().should("not.include", "/profile");
+    cy.contains("Sign in").should("exist");
+    cy.contains("About us");
+  });
 });
